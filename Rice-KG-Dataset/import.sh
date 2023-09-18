d71bbb578d5df217e41f320ab29b029c8a495543
Franck Michel <frmichel@users.noreply.github.com>
2023-09-18 16:53:19 +0200

    Update import scripts

--- Rice-KG-Dataset/import.sh
+++ Rice-KG-Dataset/import.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-# Author: Franck MICEHL, University Cote d'Azur, CNRS, Inria
-# Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 
 # Before executing this scipt, make sure to set variables:
 # - $VIRTUOSO_PWD: Virtuoso password for user dba
 # - $VIRTUOSO_CONTAINER: name of the Docker container running Virtuoso
 
+if [ -f "env.sh" ]; then
+   . ./env.sh
+fi
+
 unzip -u RiceGenomicsSLKG.zip
 
-docker exec $VIRTUOSO_CONTAINER \
+sudo docker exec $VIRTUOSO_CONTAINER \
 	isql -H localhost -U dba -P $VIRTUOSO_PWD \
-        exec="LOAD /dataset/ricegenomicsslkg/import.isql"
+        exec="LOAD /dataset_ricegenomicsslkg/import.isql"
